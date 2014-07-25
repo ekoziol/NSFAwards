3395219f45059e33dfe57828a94f1b37870d0cdf
ekoziol <e.trip.k@gmail.com>
2014-07-24 22:46:50 -0500

    Started formatting shiny app
    Still need to get data sorter to work with reactive variables.

--- server.R
+++ server.R
@@ -1,12 +1,23 @@
 library(shiny)
+library(ggmap)
 
+load("map.Rda")
+gmap <- ggmap(map)
+nsfd <- read.csv("process/processedData.csv")
+y <- 2008
+
+currentMap <- function(y){
+  return(gmap + geom_point(data = nsfd[nsfd$year == y,], aes(x=long, y=lat, color=fundsMil, size=fundsMil), na.rm=TRUE))
+}
 
 shinyServer(
   function(input,output){
     #x <- reactive({as.numeric(input$text1)+100})
-    y <- reactive(input$y)
+    y <- reactive(as.numeric(input$y))
     output$text1 <- renderText(y())
-    
+    output$nsfMap <- renderPlot({
+      currentMap(y)
+    })
     
   }
   
--- ui.R
+++ ui.R
@@ -3,12 +3,15 @@ shinyUI(pageWithSidebar(
   headerPanel("National Science Foundation Funding Breakdown"),
   sidebarPanel(
     h3('Select a year'),
-    sliderInput('y', 'Pick a year',value = 2008, min = 2005, max = 2015, step = 1)
+    sliderInput('y', '',value = 2008, min = 2005, max = 2015, step = 1, format="####")
     
   ),
   mainPanel(
-    h3('Geographic Monetary Breakdown'),
-    p('some test text'),
-    textOutput('text1')
+    h3('Geographic Funding Breakdown'),
+    p('Below you can view how the location of funding from the National Science Foundation (NSF) changes by year.
+      NSF Funding can be seen as a source of upcoming technologies that will likely be in industry in the next 
+      5 to 10 years.  Therefore, by viewing the data below, you may be able to predict the location of the next big
+      trend.'),
+    plotOutput('nsfMap')
   )
 ))
