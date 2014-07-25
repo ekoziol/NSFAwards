61e5bd61749be329309ad52c404f3d060177f2d3
Eric Koziol <e.trip.k@gmail.com>
2014-07-25 15:04:53 -0500

    Completed App

--- server.R
+++ server.R
@@ -1,24 +1,39 @@
 library(shiny)
 library(ggmap)
-library(zipcode)
+#library(zipcode)
 
-zipcode <- zipcode
-data(zipcode)
+# zipcode <- zipcode
+# data(zipcode)
 load("map.Rda")
 gmap <- ggmap(map)
-nsfd <- read.csv("process/processedData.csv")
-nsfd$fundsMil <- nsfd$funds/1000000
+nsfd <- read.csv("process/processedData.csv", colClasses=c(zip="character"))
+#nsfd$fundsMil <- nsfd$funds/1000000
+#nsfd$cityState <- sapply(nsfd$zip, findCity)
 y <- 2008
 
 currentMap <- function(y){
-  return(gmap + geom_point(data = nsfd[nsfd$year== y,], aes(x=long, y=lat, color=fundsMil, size=fundsMil), na.rm=TRUE)
-         + scale_colour_gradientn(colours=c("blue","purple","red")))
+  return(gmap + geom_point(data = nsfd[nsfd$year== y,], aes(x=long, y=lat, color=fundsMil, size=fundsMil), 
+                           na.rm=TRUE, name = "Funds Awarded($M)") + scale_size(guide = 'none')
+         + scale_colour_gradientn(colours=c("blue","purple","red"), name = "Funds Awarded($M)")
+         + theme(axis.line=element_blank(),axis.text.x=element_blank(),
+                  axis.text.y=element_blank(),axis.ticks=element_blank(),
+                  axis.title.x=element_blank(),
+                  axis.title.y=element_blank())
+  )
 }
 
-findCity <- function(z){
-  cityState <- zipcode[zipcode$zip == z, c("city", "state")]
-  return(paste(cityState[1], cityState[2], sep = ", "))
+# findCity <- function(z){
+#   cityState <- zipcode[zipcode$zip == z, c("city", "state")]
+#   return(paste(cityState[1], cityState[2], sep = ", "))
+# }
+
+findTop10 <- function(y){
+  nsfdtemp <- nsfd[nsfd$year == y,]
+  top <- head(nsfdtemp[with(nsfdtemp, order(-fundsMil)),c("cityState", "fundsMil")], 10)
+  names(top) <- c("City, State", "Funds Awarded ($Millions)")
+  return(top)
 }
+
 
 shinyServer(
   function(input,output){
@@ -28,6 +43,8 @@ shinyServer(
     output$nsfMap <- renderPlot({
       currentMap(y())
     })
+    output$top10 <- renderDataTable({
+      findTop10(y())},options = list(bFilter=0, bSort=0, bProcessing=0, bPaginate=0, bInfo=0))
     
   }
   
--- ui.R
+++ ui.R
@@ -2,8 +2,14 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("National Science Foundation Funding Breakdown"),
   sidebarPanel(
+    p('The purpose of this data application is to explore how funding allocations from 
+      the NSF across the United States change with time.  To interact with the app just change the slider to 
+      the year you wish to view.  The map will then change to show you how funds were distributed for that year.
+      Below you will find a list of the top ten cities based on funding for that year.'),
     h3('Select a year'),
-    sliderInput('y', '',value = 2008, min = 2005, max = 2014, step = 1, format="####")
+    sliderInput('y', '',value = 2010, min = 2006, max = 2014, step = 1, format="####"),
+    dataTableOutput('top10'),
+    tags$style(type="text/css", '#top10 tfoot {display:none;}')
     
   ),
   mainPanel(
@@ -11,7 +17,9 @@ shinyUI(pageWithSidebar(
     p('Below you can view how the location of funding from the National Science Foundation (NSF) changes by year.
       NSF Funding can be seen as a source of upcoming technologies that will likely be in industry in the next 
       5 to 10 years.  Therefore, by viewing the data below, you may be able to predict the location of the next big
-      trend.'),
+      trend.  The locations of the largest funding tend to be major universities but it is interesting to note how their
+      total funding changes from year to year.  The data was taken courtesy of http://www.research.gov/research-portal/appmanager/base/desktop?_nfpb=true&_eventName=viewQuickSearchFormEvent_so_rsr
+      which was found on www.data.gov.'),
     plotOutput('nsfMap', height = 800, width = 800)
   )
 ))
